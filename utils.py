59315a210571288f6a36efed0f1611f9d7184624
Beemd <60712049+Beemd@users.noreply.github.com>
2024-07-31 19:26:15 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
-# function defined to get pixel HU
+def generate_dicom_path_dict(axial_series_dir):
+    dicom_path_dict = {}
+    for root, dirs, files in os.walk(axial_series_dir):
+        for dir in dirs:
+            dicom_path_dict[dir] = os.path.join(root, dir)
+    return dicom_path_dict
+
+def get_dicom_path(nifti_parts, dicom_path_dict):
+    return dicom_path_dict.get(nifti_parts)
+
 def get_pixels_hu(scans):
     image = scans.pixel_array
     # Convert to int16 (from sometimes int16), 
